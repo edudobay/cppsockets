ab7d6d53d1753cf0908cc50fde68fd170b275d3e
Eduardo Dobay <edudobay@gmail.com>
2014-01-27 19:08:08 -0200

    add error handling to poll()

--- src/Poll.cpp
+++ src/Poll.cpp
@@ -1,10 +1,16 @@
 #include "Poll.h"
+#include "Error.h"
 
 namespace cppsockets {
 
 int poll (std::vector<pollfd>& fds, int timeout_ms)
 {
-   return ::poll(fds.data(), fds.size(), timeout_ms);
+   int result = ::poll(fds.data(), fds.size(), timeout_ms);
+   // pass-through signal interrupt 'error'
+   if (result == -1 && errno != EINTR) {
+      SOCKET_ERROR(errno, "poll");
+   }
+   return result;
 }
 
 } // namespace cppsockets
